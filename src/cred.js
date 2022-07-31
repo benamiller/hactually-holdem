9b4e7176c14c506cf84ca29ca473426da30c9992
Hactually Benji <mrbenamiller@gmail.com>
2022-07-31 10:02:26 -0700

    20: Add auth component

---
+++ src/cred.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyBIrBy4hAi7BJyObpdM7nEg-Z514Y6pyXE",
+    authDomain: "hactually-holdem.firebaseapp.com",
+    databaseURL: "https://hactually-holdem-default-rtdb.firebaseio.com",
+    projectId: "hactually-holdem",
+    storageBucket: "hactually-holdem.appspot.com",
+    messagingSenderId: "18505024517",
+    appId: "1:18505024517:web:a43b2e0d383204b1e9c86c",
+    measurementId: "G-KL9THYD2YJ",
+}
+
+export default config;
